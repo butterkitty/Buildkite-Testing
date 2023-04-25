99a5bf5485d757a4933f52e54a2dd75cd7448287
butterkitty <plarina@gmail.com>
2023-04-25 16:42:01 -0400

    asd

--- test.sh
+++ test.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 echo $UID 
 ls -l /home/michelle-work
+
+function inline_link {
+  LINK=$(printf "url='%s'" "$1")
+
+  if [ $# -gt 1 ]; then
+    LINK=$(printf "$LINK;content='%s'" "$2")
+  fi
+
+  printf '\033]1339;%s\a\n' "$LINK"
+}
+
+inline_link "https://buildkite.com/%20" "test"
