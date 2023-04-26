86a1743c342b84c00c6db95a9440138f00b0f799
butterkitty <plarina@gmail.com>
2023-04-26 08:37:02 -0400

    asd

--- test.sh
+++ test.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 echo $UID 
 ls -l /home/michelle-work 
+
+function inline_link {
+  LINK=$(printf "url='%s'" "$1")
+
+  if [ $# -gt 1 ]; then
+    LINK=$(printf "%s;content='%s'" "$LINK" "$2")
+  fi
+
+  printf '\033]1339;%s\a\n' "$LINK"
+}
