d70446a43dfb61f9494b65d980cca1494fd3c058
Michelle <butterkitty@users.noreply.github.com>
2023-06-01 12:56:33 -0400

    Update test.sh
    change to download git repo

--- test.sh
+++ test.sh
@@ -2,16 +2,21 @@
 
 set -euo pipefail
 
-NODE_VERSION=v18.16.0
-NVM_VERSION=v0.39.0
+apt install -y git
+
+git clone https://github.com/buildkite/agent-stack-k8s.git
 
 
-curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin
+#NODE_VERSION=v18.16.0
+#NVM_VERSION=v0.39.0
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/${NVM_VERSION}/install.sh | bash
 
-export NVM_DIR="$HOME/.nvm" && \
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && \
-    nvm install $NODE_VERSION && \
-    nvm alias default $NODE_VERSION && \
-    nvm use default
+#curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin
+
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/${NVM_VERSION}/install.sh | bash
+
+#export NVM_DIR="$HOME/.nvm" && \
+#    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && \
+#    nvm install $NODE_VERSION && \
+#    nvm alias default $NODE_VERSION && \
+#    nvm use default
